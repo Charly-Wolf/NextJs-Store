2bcc07aed3fcccd4b7737f5aa83bb0bdc841221a
Charly <cardan1990@hotmail.com>
2024-08-16 11:23:30 +0200

    Added shadcn components

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
+import { Button } from '@/components/ui/button'
+
 const HomePage = () => {
-  return <div>HomePage</div>
+  return (
+    <div>
+      <h1 className='text-3xl'>Home</h1>
+    </div>
+  )
 }
 export default HomePage
