f71efb03a4bcfd6597209f4dc94368f8ccfebb80
Charly <cardan1990@hotmail.com>
2024-08-16 11:52:07 +0200

    Setup Logo component

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,6 +1,14 @@
+import { FaStoreAlt } from 'react-icons/fa'
+import { Button } from '../ui/button'
+import Link from 'next/link'
+
 const Logo = () => {
   return (
-    <div>Logo</div>
+    <Button size='icon' asChild>
+      <Link href='/'>
+        <FaStoreAlt />
+      </Link>
+    </Button>
   )
 }
 export default Logo
